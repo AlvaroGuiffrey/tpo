3fa2d2f5f78e924631bcace5615744feaf40c878
AlvaroGuiffrey <alvaroguiffrey@gmail.com>
2023-05-24 13:47:19 -0300

    Cambio tpo a AlmaBichera

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ import { Experiencia } from "./models/Experiencia.js";
 import experiancias from "../../db/experiencias.json" assert { type: "json" };
 import { idValueInput, parent } from "./config/config.js";
 
+/* EXPERIENCIAS */
 const printDatos = (datos, parent) => {
     datos.forEach((expe) => expe.getDivCardExperiencia(parent));
 };
@@ -18,7 +19,26 @@ let datos = cargarDatos();
 
 printDatos(datos, parent);
 
-
+/* CARRUSEL */
+let slideIndex = 0;
+    showSlides();
+    let x = 1;
+    console.log(x);
+    
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+     
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1};    
+      
+      slides[slideIndex-1].style.display = "block";  
+      
+      setTimeout(showSlides, 3000); // Change image every 3 seconds
+    }
 
 
 
